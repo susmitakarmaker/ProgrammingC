2d238ce84343a1d7dc4a0381eab6fc6425782022
susmita66 <susmitakarmaker69@gmail.com>
2024-08-10 19:52:35 +0600

    code-1

--- main.c1.c
+++ main.c1.c
@@ -2,9 +2,20 @@
 
 int main()
 {
+    int n,row,col;
+    printf("Enter the number : ");
+    scanf("%d",&n);
 
-    printf("Ascii value of 'A' is : %d\n",'A');
-    printf("Ascii value of '@' is : %d\n",'@');
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=n-row;col++)
+            printf(" ");
+        for(col=1;col<=row;col++)
+            printf("%d ",col);
+
+        printf("\n");
+    }
+
 
 
     return 0;
