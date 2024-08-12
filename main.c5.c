ae491acff9c26ca927e59417a6fdd155663670d2
susmita66 <susmitakarmaker69@gmail.com>
2024-08-12 13:51:43 +0600

    code-5

---
+++ main.c5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void number(int row)
+{
+    for(int i=1;i<=row;i++)
+    {
+        for(int j=1;j<=row-i;j++)
+        {
+            printf(" ");
+        }
+        for(int star=1;star<=2*i-1;star++)
+        {
+            printf("*");
+        }
+        for(int j=1;j<=row-i;j++)
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    number(n);
+
+    return 0;
+}
